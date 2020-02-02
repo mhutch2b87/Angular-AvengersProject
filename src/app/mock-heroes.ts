fda34a8b12d075b7deef1777d8afeed0ce77ac35
mhutch2b87 <mhutch2b87@gmail.com>
2020-02-02 16:09:42 -0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Thor' },
+  { id: 12, name: 'Scarlett Witch' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Black Widow' },
+  { id: 16, name: 'Hulk' },
+  { id: 17, name: 'HawkEye' },
+  { id: 18, name: 'Doctor Strange' },
+  { id: 19, name: 'Spider Man' },
+  { id: 20, name: 'Vision' }
+];
